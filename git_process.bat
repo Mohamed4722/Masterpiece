801e5afc77499de4b44ae87445280870cda058b5
ELWESHAHY Mohamed <mohamed.elweshahy@egis-group.com>
2022-08-30 23:27:38 +0200

    Second commit

---
+++ git_process.bat
@@ -0,0 +1,9 @@
+git init
+git add chapter1.txt
+git commit -m "Initial commit"
+git log 
+:: to check the commits ::::::::::::::::::::::
+:: ensuite je créee chapter2 et 3 ...:::::::::::::::::::
+::git add . 
+:: pour ajouter tous les nouveaux fichiers puis: ::::::::::::::
+::git commit -m "Second commit with new chapters"
